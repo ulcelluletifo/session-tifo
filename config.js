a94c0dff54f89ed8437c300960086391d35d5d05
ulcelluletifo <ulcelluletifo@gmail.com>
2026-06-13 04:13:57 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,15 +1,5 @@
 /* =====================================================================
    config.js — LE SEUL FICHIER À MODIFIER
-   ---------------------------------------------------------------------
-   Colle ci-dessous l'URL de TON déploiement Apps Script (terminant
-   par /exec). Tu l'obtiens dans Apps Script :
-   Déployer ▸ Nouveau déploiement ▸ Application Web ▸ (copier l'URL).
    ===================================================================== */
 
-window.TIFO_API_URL = "https://script.google.com/macros/s/AKfycbwXV3dj4hI9QuZQT1an9MXS1H3g40NzvDJJMU8v8HVngdZ-COj88qqEUCyahepWa-83/exec";
-
-
-
-/* Exemple :
-window.TIFO_API_URL = "https://script.google.com/macros/s/AKfycbxXXXXXXXXXXXXXXXXXX/exec";
-*/
+window.TIFO_API_URL = "https://script.google.com/macros/s/AKfycbyRNL84c2I_FnFKABuWWP0oi0kqW6DfKDdmDs-aOlrqkkWePZK__CZZcNs13Uw5CFjr/exec";
